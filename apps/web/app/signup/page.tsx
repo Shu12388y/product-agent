0763fe995a5163548431ddf05d514ec1da8f6edf
shu12388y <spl.sp999@gmail.com>
2025-12-30 20:58:07 +0530

    added the ui

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -2,7 +2,7 @@ import { GalleryVerticalEnd } from "lucide-react"
 
 import { LoginForm } from "@/components/login-form"
 
-export default function Page() {
+export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
@@ -22,7 +22,7 @@ export default function Page() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="https://images.unsplash.com/photo-1544819667-9bfc1de23d4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHByb2R1Y3Rpdml0eXxlbnwwfHwwfHx8MA%3D%3D"
+          src="/placeholder.svg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
